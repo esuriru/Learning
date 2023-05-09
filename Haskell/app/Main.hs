1fcbbc2f85a7f09cf21f905d76d3fd255f68edd3
Christian <97773844+esuriru@users.noreply.github.com>
2023-05-09 17:52:03 +0800

    Yeah!

--- Haskell/app/Main.hs
+++ Haskell/app/Main.hs
@@ -2,7 +2,17 @@ module Main where
 
 import qualified MyLib (someFunc)
 
+isLetterX :: Char -> Bool
+isLetterX c = c == 'x'
+
 main :: IO ()
 main = do
-  putStrLn "Hello, Haskell!"
-  MyLib.someFunc
+    putStrLn "Hello, Haskell!"
+    MyLib.someFunc
+    c <- getChar
+    if isLetterX c 
+        then do 
+            putStrLn "The letter is x!" 
+        else do 
+            putStrLn "The letter is not x!"
+    
